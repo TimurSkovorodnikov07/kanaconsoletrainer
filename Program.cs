7475478cdae1dc1fe0b39ebbde3640a2de0ed808
TimurSkovorodnikov07 <skovorodnikovtimur7001@gmail.com>
2024-12-26 12:42:00 +0500

    added-guess-mode

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿public static class Program
+﻿namespace kanaTrainer;
+
+public static class Program
 {
-    private static readonly Dictionary<string, string> _hiraganaDictionary = new Dictionary<string, string>
+    private static readonly Dictionary<string, string> HiraganaDictionary = new()
     {
         { "あ", "a" }, { "い", "i" }, { "う", "u" }, { "え", "e" }, { "お", "o" },
         { "か", "ka" }, { "き", "ki" }, { "く", "ku" }, { "け", "ke" }, { "こ", "ko" },
@@ -12,6 +14,10 @@
         { "や", "ya" }, { "ゆ", "yu" }, { "よ", "yo" },
         { "ら", "ra" }, { "り", "ri" }, { "る", "ru" }, { "れ", "re" }, { "ろ", "ro" },
         { "わ", "wa" }, { "を", "wo" }, { "ん", "n" },
+    };
+
+    private static readonly Dictionary<string, string> HiraganaDakutenAndHandukenDictionary = new()
+    {
         { "が", "ga" }, { "ぎ", "gi" }, { "ぐ", "gu" }, { "げ", "ge" }, { "ご", "go" },
         { "ざ", "za" }, { "じ", "ji" }, { "ず", "zu" }, { "ぜ", "ze" }, { "ぞ", "zo" },
         { "だ", "da" }, { "ぢ", "ji" }, { "づ", "dzu" }, { "で", "de" }, { "ど", "do" },
@@ -19,7 +25,8 @@
         { "ぱ", "pa" }, { "ぴ", "pi" }, { "ぷ", "pu" }, { "ぺ", "pe" }, { "ぽ", "po" }
     };
 
-    private static readonly Dictionary<string, string> _katakanaDictionary = new Dictionary<string, string>
+
+    private static readonly Dictionary<string, string> KatakanaDictionary = new()
     {
         { "ア", "a" }, { "イ", "i" }, { "ウ", "u" }, { "エ", "e" }, { "オ", "o" },
         { "カ", "ka" }, { "キ", "ki" }, { "ク", "ku" }, { "ケ", "ke" }, { "コ", "ko" },
@@ -31,12 +38,18 @@
         { "ヤ", "ya" }, { "ユ", "yu" }, { "ヨ", "yo" },
         { "ラ", "ra" }, { "リ", "ri" }, { "ル", "ru" }, { "レ", "re" }, { "ロ", "ro" },
         { "ワ", "wa" }, { "ヲ", "wo" }, { "ン", "n" },
+    };
+
+    private static readonly Dictionary<string, string> KatakanaDakutenAndHandukenDictionary = new()
+    {
         { "ガ", "ga" }, { "ギ", "gi" }, { "グ", "gu" }, { "ゲ", "ge" }, { "ゴ", "go" },
         { "ザ", "za" }, { "ジ", "ji" }, { "ズ", "zu" }, { "ゼ", "ze" }, { "ゾ", "zo" },
         { "ダ", "da" }, { "ヂ", "ji" }, { "ヅ", "dzu" }, { "デ", "de" }, { "ド", "do" },
         { "バ", "ba" }, { "ビ", "bi" }, { "ブ", "bu" }, { "ベ", "be" }, { "ボ", "bo" },
         { "パ", "pa" }, { "ピ", "pi" }, { "プ", "pu" }, { "ペ", "pe" }, { "ポ", "po" }
     };
+
+    private static readonly Dictionary<string, string> WrongAnswers = new() { };
 
     private enum Alphabet
     {
@@ -46,46 +59,115 @@
     }
 
     private static Alphabet _currentAlphabet = Alphabet.None;
+    private static bool _guessMode;
 
     public static void Main(string[] args)
     {
-        Write("仮名 console  trainer");
-        Write("To change the alphabet use the command: 'change'");
-        Thread.Sleep(3000);
+        Write("Kana console  trainer");
+        Thread.Sleep(1000);
         Console.Clear();
+
         ChooseAlphabet();
         Console.Clear();
+
+        if (args.Contains("-d") == false || args.Contains("-e"))
+        {
+            Write("With the Dakuten and handakuten? If so, write 'y'", ConsoleColor.Magenta);
+            var outputKey = Console.ReadKey();
+            if (outputKey.KeyChar == 'y' || outputKey.KeyChar == 'Y')
+            {
+                foreach (var x in HiraganaDakutenAndHandukenDictionary)
+                    HiraganaDictionary.Add(x.Key, x.Value);
+                foreach (var y in KatakanaDakutenAndHandukenDictionary)
+                    KatakanaDictionary.Add(y.Key, y.Value);
+            }
+        }
+        Console.Clear();
+
+        _guessMode = args.Contains("-g");
+        if (_guessMode == false)
+        {
+            Write("Enable a guess mode?  If so, write 'y'", ConsoleColor.Gray);
+            var guessModeKeyChar = Console.ReadKey().KeyChar;
+            _guessMode = guessModeKeyChar == 'y' || guessModeKeyChar == 'Y';
+        }
+        Console.Clear();
+        
+        Console.WriteLine("\n");
 
         while (true)
         {
             var randomLetter = GetRangomLetter();
-            Write($"{randomLetter.Key}\n", ConsoleColor.Cyan);
+            Write($"{randomLetter.Key}", ConsoleColor.Cyan);
+
+            if (_guessMode)
+            {
+                var maxLetters = 5;
+                var randomNumber = new Random().Next(0, maxLetters);
+                var variantsString = " ";
+
+                for (int i = 0; i < randomNumber; i++)
+                    variantsString += $"[{GetRangomLetter(false, randomLetter.Key).Value}] ";
+
+                variantsString += $"[{randomLetter.Value}] ";
+
+                for (int i = randomNumber; i < maxLetters - 1; i++)
+                    variantsString += $"[{GetRangomLetter(false, randomLetter.Key).Value}] ";
+
+                Write($"{variantsString}", ConsoleColor.Yellow);
+            }
 
             var letter = Console.ReadLine();
-
             if (letter == randomLetter.Value)
-                Write("GREAT!!!", ConsoleColor.Green, true);
-            else if (letter == "change")
-                ChooseAlphabet();
-            else if (letter == "clear")
-                Console.Clear();
-            else
-                Write($"WRONG!!! The right answer: {randomLetter.Key}({randomLetter.Value})", ConsoleColor.Red, true);
+            {
+                WriteDashesLine(ConsoleColor.Green);
 
-            WriteDashesLine();
+                WrongAnswers.Remove(randomLetter.Key);
+            }
+            else if (letter == "change")
+            {
+                ChooseAlphabet();
+                Console.Clear();
+            }
+            else if (letter == "clear")
+            {
+                Console.Clear();
+            }
+            else
+            {
+                var wrongColor = ConsoleColor.Red;
+
+                Write($"{randomLetter.Key}({randomLetter.Value})", wrongColor);
+                WriteDashesLine(wrongColor);
+
+                if (WrongAnswers.ContainsKey(randomLetter.Key) == false)
+                    WrongAnswers.Add(randomLetter.Key, randomLetter.Value);
+            }
         }
     }
 
-    private static KeyValuePair<string, string> GetRangomLetter()
+    private static KeyValuePair<string, string> GetRangomLetter(bool withWrongAnswer = true,
+        string forbiddenLetter = "")
     {
-        if (_currentAlphabet == Alphabet.Hiragana)
-        {
-            var randomIndex = new Random().Next(0, _hiraganaDictionary.Count);
-            return _hiraganaDictionary.ElementAt(randomIndex);
-        }
+        var result = KeyValuePair.Create(forbiddenLetter, string.Empty);
 
-        var randomIndexFromKatakana = new Random().Next(0, _katakanaDictionary.Count);
-        return _katakanaDictionary.ElementAt(randomIndexFromKatakana);
+        while (result.Key == forbiddenLetter)
+        {
+            if (withWrongAnswer && WrongAnswers.Count > 0)
+            {
+                var returnWrongAnswer = new Random().Next(0, 2);
+
+                if (returnWrongAnswer == 0)
+                {
+                    result = WrongAnswers.ElementAt(WrongAnswers.GetRandomIndex());
+                    continue;
+                }
+            }
+            result = _currentAlphabet == Alphabet.Hiragana
+                ? HiraganaDictionary.ElementAt(HiraganaDictionary.GetRandomIndex())
+                : KatakanaDictionary.ElementAt(KatakanaDictionary.GetRandomIndex());
+        }
+        return result;
     }
 
     private static void ChooseAlphabet()
@@ -95,7 +177,6 @@
         while (_currentAlphabet == Alphabet.None)
         {
             Write("Choose alphabet: ", ConsoleColor.Green);
-            Thread.Sleep(1000);
             Console.WriteLine("Hiragana: 1 or h");
             Console.WriteLine("Katakana: 2 or k");
 
@@ -105,14 +186,19 @@
                 (keyChar == 'h' || keyChar == '1') ? Alphabet.Hiragana :
                 (keyChar == 'k' || keyChar == '2') ? Alphabet.Katakana :
                 Alphabet.None;
+
+            if (_currentAlphabet is not Alphabet.None)
+            {
+                WrongAnswers.Clear();
+            }
         }
     }
 
-    private static void Write(string text, ConsoleColor color = ConsoleColor.White, bool withEnterAfter = false)
+    private static void Write(string text, ConsoleColor color = ConsoleColor.White)
     {
-        int consoleWidth = Console.WindowWidth;
-        int textStartPosition = (consoleWidth - text.Length) / 2;
-        string spaces = textStartPosition > 0 ? new string(' ', textStartPosition) : "";
+        var consoleWidth = Console.WindowWidth;
+        var textStartPosition = (consoleWidth - text.Length) / 2;
+        var spaces = textStartPosition > 0 ? new string(' ', textStartPosition) : "";
 
 
         var defColor = Console.ForegroundColor;
@@ -121,14 +207,12 @@
         Console.WriteLine(spaces + text);
 
         Console.ForegroundColor = defColor;
-
-        if (withEnterAfter) Console.WriteLine("\n");
     }
-    
+
     private static void WriteDashesLine(ConsoleColor color = ConsoleColor.White)
     {
-        int consoleWidth = Console.WindowWidth;
-        string dashes = consoleWidth > 2 ? new string('-', consoleWidth - 2) : null;
+        var consoleWidth = Console.WindowWidth;
+        var dashes = consoleWidth > 2 ? new string('-', consoleWidth - 2) : null;
 
         if (dashes != null)
             Write(dashes, color);
--- obj/Debug/net8.0/kanaTrainer.AssemblyInfo.cs
+++ obj/Debug/net8.0/kanaTrainer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kanaTrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aae6789d418afbd1d14519f382b34a66adc716e")]
 [assembly: System.Reflection.AssemblyProductAttribute("kanaTrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kanaTrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
